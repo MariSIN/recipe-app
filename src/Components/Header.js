7aef9604f67d39526893ddc1883aeb69e3f00360
Roger Marques <marquesroger226@gmail.com>
2022-11-29 18:02:16 -0300

    [feat]: Requisito 11

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,14 +1,40 @@
 import PropTypes from 'prop-types';
-import { useState } from 'react';
+import { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
+import Context from '../Context/Context';
 import profile from '../images/profileIcon.svg';
 import search from '../images/searchIcon.svg';
 
 function Header({ title }) {
+  const { handleChange, searchFilter, setSearchResult } = useContext(Context);
   const [show, setShow] = useState(false);
 
   const showSearch = () => {
     setShow(!show);
+  };
+
+  const fetchAPI = async () => {
+    if (searchFilter.selectedFilter === 'Ingredient') {
+      const url = `https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchFilter.searchName}`;
+      const response = await fetch(url);
+      const data = await response.json();
+      setSearchResult(data);
+    }
+    if (searchFilter.selectedFilter === 'Name') {
+      const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchFilter.searchName}`;
+      const response = await fetch(url);
+      const data = await response.json();
+      setSearchResult(data);
+    }
+    if (searchFilter.selectedFilter === 'First Letter') {
+      if (searchFilter.searchName.length > 1) {
+        return global.alert('Your search must have only 1 (one) character');
+      }
+      const url = `https://www.themealdb.com/api/json/v1/1/search.php?f=${searchFilter.searchName}`;
+      const response = await fetch(url);
+      const data = await response.json();
+      setSearchResult(data);
+    }
   };
 
   return (
@@ -29,13 +55,22 @@ function Header({ title }) {
       </button>
       {show && (
         <div>
-          <input data-testid="search-input" type="text" placeholder="Search Recipe" />
+          <input
+            data-testid="search-input"
+            type="text"
+            name="searchName"
+            value={ searchFilter.searchName }
+            onChange={ handleChange }
+            placeholder="Search Recipe"
+          />
           <label htmlFor="ingredient">
             Ingredient
             <input
               data-testid="ingredient-search-radio"
               type="radio"
-              name="ingredient"
+              name="selectedFilter"
+              value="Ingredient"
+              onChange={ handleChange }
               id="ingredient"
             />
           </label>
@@ -44,7 +79,9 @@ function Header({ title }) {
             <input
               data-testid="name-search-radio"
               type="radio"
-              name="name"
+              name="selectedFilter"
+              value="Name"
+              onChange={ handleChange }
               id="name"
             />
           </label>
@@ -53,11 +90,20 @@ function Header({ title }) {
             <input
               data-testid="first-letter-search-radio"
               type="radio"
-              name="first-letter"
+              name="selectedFilter"
+              value="First Letter"
+              onChange={ handleChange }
               id="first-letter"
             />
           </label>
-          <button data-testid="exec-search-btn" type="button">Search</button>
+          <button
+            data-testid="exec-search-btn"
+            type="button"
+            onClick={ fetchAPI }
+          >
+            Search
+
+          </button>
         </div>
       )}
     </header>
