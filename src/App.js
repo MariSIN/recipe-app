b5fdbbf6616ce1a3b1c159ac6d4bb74bc2584efa
Roger Marques <marquesroger226@gmail.com>
2022-11-29 18:52:00 -0300

    [feat]: Requisito 12

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
+import Drinks from './Pages/Drinks';
 import Login from './Pages/Login';
 import Meals from './Pages/Meals';
 import Profile from './Pages/Profile';
@@ -12,7 +13,7 @@ function App() {
       <Route exact path="/" component={ Login } />
       <Route path="/profile" component={ Profile } />
       <Route path="/meals" component={ Meals } />
-
+      <Route path="/drinks" component={ Drinks } />
     </Switch>
   );
 }
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,37 +4,14 @@ import { Link } from 'react-router-dom';
 import Context from '../Context/Context';
 import profile from '../images/profileIcon.svg';
 import search from '../images/searchIcon.svg';
+import SearchBar from './SearchBar';
 
 function Header({ title }) {
-  const { handleChange, searchFilter, setSearchResult } = useContext(Context);
+  const { handleChange, searchFilter } = useContext(Context);
   const [show, setShow] = useState(false);
 
   const showSearch = () => {
     setShow(!show);
-  };
-
-  const fetchAPI = async () => {
-    if (searchFilter.selectedFilter === 'Ingredient') {
-      const url = `https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchFilter.searchName}`;
-      const response = await fetch(url);
-      const data = await response.json();
-      setSearchResult(data);
-    }
-    if (searchFilter.selectedFilter === 'Name') {
-      const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchFilter.searchName}`;
-      const response = await fetch(url);
-      const data = await response.json();
-      setSearchResult(data);
-    }
-    if (searchFilter.selectedFilter === 'First Letter') {
-      if (searchFilter.searchName.length > 1) {
-        return global.alert('Your search must have only 1 (one) character');
-      }
-      const url = `https://www.themealdb.com/api/json/v1/1/search.php?f=${searchFilter.searchName}`;
-      const response = await fetch(url);
-      const data = await response.json();
-      setSearchResult(data);
-    }
   };
 
   return (
@@ -63,47 +40,7 @@ function Header({ title }) {
             onChange={ handleChange }
             placeholder="Search Recipe"
           />
-          <label htmlFor="ingredient">
-            Ingredient
-            <input
-              data-testid="ingredient-search-radio"
-              type="radio"
-              name="selectedFilter"
-              value="Ingredient"
-              onChange={ handleChange }
-              id="ingredient"
-            />
-          </label>
-          <label htmlFor="name">
-            Name
-            <input
-              data-testid="name-search-radio"
-              type="radio"
-              name="selectedFilter"
-              value="Name"
-              onChange={ handleChange }
-              id="name"
-            />
-          </label>
-          <label htmlFor="first-letter">
-            First Letter
-            <input
-              data-testid="first-letter-search-radio"
-              type="radio"
-              name="selectedFilter"
-              value="First Letter"
-              onChange={ handleChange }
-              id="first-letter"
-            />
-          </label>
-          <button
-            data-testid="exec-search-btn"
-            type="button"
-            onClick={ fetchAPI }
-          >
-            Search
-
-          </button>
+          <SearchBar title={ title } />
         </div>
       )}
     </header>
