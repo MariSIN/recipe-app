d215dc349add51492213539f5853fd04f3a7bf14
Roger Marques <marquesroger226@gmail.com>
2022-11-29 19:39:34 -0300

    [feat]: Requisito 13

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
+import Details from './Components/Details';
 import Drinks from './Pages/Drinks';
 import Login from './Pages/Login';
 import Meals from './Pages/Meals';
@@ -14,6 +15,8 @@ function App() {
       <Route path="/profile" component={ Profile } />
       <Route path="/meals" component={ Meals } />
       <Route path="/drinks" component={ Drinks } />
+      <Route path="/meals/:id-meals" component={ Details } />
+      <Route path="/drinks/:id-drink" component={ Details } />
     </Switch>
   );
 }
