ce269458d300700fb816b7102ae8a3a0587b2ce6
Roger Marques <marquesroger226@gmail.com>
2022-11-30 09:44:29 -0300

    [feat]: Requisito 18

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,11 +1,16 @@
+import { Link } from 'react-router-dom';
 import drinkIcon from '../images/drinkIcon.svg';
 import mealIcon from '../images/mealIcon.svg';
 
 function Footer() {
   return (
     <footer data-testid="footer" style={ { position: 'fixed', bottom: '0px' } }>
-      <img data-testid="meals-bottom-btn" src={ mealIcon } alt="mealIcon" />
-      <img data-testid="drinks-bottom-btn" src={ drinkIcon } alt="drinkIcon" />
+      <Link to="/meals">
+        <img data-testid="meals-bottom-btn" src={ mealIcon } alt="mealIcon" />
+      </Link>
+      <Link to="/drinks">
+        <img data-testid="drinks-bottom-btn" src={ drinkIcon } alt="drinkIcon" />
+      </Link>
     </footer>
   );
 }
