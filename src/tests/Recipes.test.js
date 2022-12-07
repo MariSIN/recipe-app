8ece0b02a1a336e57792c4002ace8c3e983cc98c
Leonardo Elias Souza de Oliverira Meireles <leonardoelias802@gmail.com>
2022-12-07 17:30:38 -0300

    fazendo teste

--- src/tests/Recipes.test.js
+++ src/tests/Recipes.test.js
@@ -1,14 +1,12 @@
 import { screen } from '@testing-library/react';
 import React from 'react';
-// import userEvent from '@testing-library/user-event';
-// import App from '../App';
 import ContextProvider from '../Context/ContextProvider';
 import renderWithRouter from '../renderWithRouter';
-// import Meals from '../Pages/Meals';
-// import { meals } from '../../cypress/mocks/meals';
 import mealCategories from '../../cypress/mocks/mealCategories';
 import Recipes from '../Pages/Recipes';
-// import { placeholderEmail, placeholderSenha, email } from './dataBase';
+import { DRINK_RECIPES, MEALS_RECIPES } from '../utilit/globalVariables';
+import drinkCategories from '../../cypress/mocks/drinkCategories';
+
 
 describe('Testando pagina Recipes parte Meals', () => {
   test('testando se há os botão na pagina meals,', async () => {
@@ -17,16 +15,39 @@ describe('Testando pagina Recipes parte Meals', () => {
     });
     renderWithRouter(
       <ContextProvider>
-        <Recipes />
-        ,
+        <Recipes endpoit={ MEALS_RECIPES } chave="meals" />
       </ContextProvider>,
-      '/meals',
+
     );
     const bottaoALL = screen.getByTestId('All-category-filter');
     expect(bottaoALL).toBeInTheDocument();
-    // userEvent.click(bottaoALL);
-    // const botaoBeef = findByTestId('Beef-category-filter');
     const titulo = screen.getByRole('heading', { name: 'Recipes', level: 1 });
     expect(titulo).toBeInTheDocument();
+    expect(await screen.findByRole('button', { name: 'Beef' })).toBeInTheDocument();
+    expect(await screen.findByRole('button', { name: 'Breakfast' })).toBeInTheDocument();
+    expect(await screen.findByRole('button', { name: 'Chicken' })).toBeInTheDocument();
+    expect(await screen.findByRole('button', { name: 'Goat' })).toBeInTheDocument();
+  });
+});
+
+describe('Testando pagina Recipes parte Drink', () => {
+  test('testando se há os botão na pagina drinks,', async () => {
+    jest.spyOn(global, 'fetch').mockResolvedValue({
+      json: jest.fn().mockResolvedValue(drinkCategories),
+    });
+    renderWithRouter(
+      <ContextProvider>
+        <Recipes endpoit={ DRINK_RECIPES } chave="drinks" />
+      </ContextProvider>,
+
+    );
+    const bottaoALL = screen.getByTestId('All-category-filter');
+    expect(bottaoALL).toBeInTheDocument();
+    const titulo = screen.getByRole('heading', { name: 'Recipes', level: 1 });
+    expect(titulo).toBeInTheDocument();
+    expect(await screen.findByRole('button', { name: 'Ordinary Drink' })).toBeInTheDocument();
+    expect(await screen.findByRole('button', { name: 'Cocktail' })).toBeInTheDocument();
+    expect(await screen.findByRole('button', { name: 'Shake' })).toBeInTheDocument();
+    expect(await screen.findByRole('button', { name: 'Other/Unknown' })).toBeInTheDocument();
   });
 });
